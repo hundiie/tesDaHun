732498b4c474c3ea24cc8b1dc2f5702672924aec
leedahun <ldh1923@naver.com>
2022-05-10 10:43:42 +0900

    게임 루프 구성

--- ConsoleApplication1/Framework/App.c
+++ ConsoleApplication1/Framework/App.c
@@ -4,9 +4,28 @@ bool App_Init()
 {
 	return true;
 }
-
-__int32 App_Run()
+void processInput()
 {
+
+}
+void update()
+{
+	
+}
+void render()
+{
+	//system("cls");
+	puts("hello Game");
+}
+int32 App_Run()
+{
+	// Game Loop : 게임을 동작시키는 루프. 프레임(Frame)이라고 한다.
+	while (true)
+	{
+		processInput();      // 입력 처리
+		update();         // 게임 업데이트
+		render();         // 게임 출력
+	}
 
 	return 0;
 }
